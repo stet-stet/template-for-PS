71b2a2d16f1f193a7f2562446261b023d78bd4ff
stet-stet <jayeonyi@snu.ac.kr>
2021-05-30 14:30:48 +0000

    wrote script for fast serving

--- serve.py
+++ serve.py
@@ -5,21 +5,22 @@ import re
 def run(filename,outfilename):
     with open(filename,'r') as file:
         r = file.read()
-    crit = re.compile(r'#include"(.*)"')
-    for match in re.finditer(crit,r):
+    crit = re.compile(r'#include"(.*?)"')
+    for match in crit.finditer(r):
         header_name = match.group(1)
         matched_string = match.group(0)
+        print(matched_string)
         try:
             with open(header_name,'r') as file:
                 header_content = file.read()
-            r.replace(matched_string,header_content)
+            r = r.replace(matched_string,header_content)
         except FileNotFoundError:
-            pass
+            print(header_name+" is not found within folder")
     with open(outfilename,'w') as file:
         file.write(r)
 
 if __name__=="__main__":
-    if len(sys)<3:
+    if len(sys.argv)<3:
         print("usage: python "+sys.argv[0]+" (input) (output)")
     else:
         run(sys.argv[1],sys.argv[2])
